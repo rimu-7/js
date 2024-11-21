fed31eae035165b195bb56a276943b169448a743
Mutasim Fuad Rimu <108598491+rimu-7@users.noreply.github.com>
2024-11-21 06:36:08 +0000

    datatype

---
+++ basics/2_datatype.js
@@ -0,0 +1,12 @@
+"use strict" //treat all JS code in new versions
+
+console.log(3 + 3);
+
+console.log("hello world");
+let isLogin = true
+let state;
+let int = 2;
+
+console.log(typeof null);
+
+console.log(typeof undefined);
