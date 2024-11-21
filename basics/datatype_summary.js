bc29bd8c6eb9051c57fc9f48acd7d9364a0e57f9
Mutasim Fuad Rimu <108598491+rimu-7@users.noreply.github.com>
2024-11-21 08:19:49 +0000

    datatype summary

--- basics/datatype_summary.js
+++ basics/datatype_summary.js
@@ -9,7 +9,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 let bigNumber = 63457823475683746578345783478n
 
@@ -25,11 +25,40 @@ const myFunction = function(){
     
 }
 
-console.log(typeof bigNumber);
-console.log(typeof anotherId);
+// console.log(typeof bigNumber);
+// console.log(typeof anotherId);
 
 
 
 
 // Reference (Non primitive)
 //Aray, Objects, Functions
+
+// stack(primitive)  heap(non primitive)
+// all the primitive is going to the stack(numbers, boolean)
+//all the array, objects, functions going to the heaps
+
+
+let myName = "Fuad"
+let chineseName = myName
+
+chineseName = "fu yuan"
+
+console.log(myName);
+console.log(chineseName);
+
+
+let user1 = {
+    name: 'fuad',
+    email: 'fuad@proton.me',
+}
+
+let user2 = {
+    name: 'jesse',
+    email: 'jessebhai7@gmail.com'
+}
+
+user2.name = 'rimu'
+
+console.log(user1.name);
+console.log(user2.name);
